003fe837dadf51f3a903489e45b96182066d0287
LizandroV <lizandrovivanco16@gmail.com>
2023-09-20 18:13:49 -0500

    doing practice of week2

---
+++ Week02-DOM/w02-practice.js
@@ -0,0 +1,32 @@
+let x = 3;
+let y = 5;
+//Add two numbers
+console.log(x + y);
+//Multiply two numbers
+console.log(x * y);
+//Subtract one from a number
+console.log(x - y);
+//concatinate two strings together.
+let word1 = "Hola";
+let word2 = "Mundo";
+console.log(word1 + " " + word2);
+//assign a value to a variable
+let w = 5;
+let z = 0;
+z = ++w;
+console.log(z);
+console.log("w = " + w);
+//increment the value in a variable by 3
+z +=3
+console.log("z = " + z);
+//compare two values to see if they are the same
+console.log(w === z);
+//check to see if one number is less than another number
+console.log(w < z);
+//Check to see if two values are NOT equal
+console.log(w !== z);
+//check to see if a value is less than 10 and greater than 0
+console.log(w < 10 && w > 0);
+
+console.log("-----------");
+console.log("a" < "b");
---
+++ scripts/w02-task.js
@@ -0,0 +1,30 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = "Lizandro Vivanco";
+const currentYear = 2023;
+const profilePicture = "images/IMG_20211214.jpg"
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img"); 
+img.src = profilePicture;
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+

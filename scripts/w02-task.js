e606aadbff9cd18dc7a199b97324840eab97082f
LizandroV <lizandrovivanco16@gmail.com>
2023-09-21 11:36:47 -0500

    i have finished the task, i am so happy.

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,28 +3,30 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-const fullName = "Lizandro Vivanco";
-const currentYear = 2023;
-const profilePicture = "images/IMG_20211214.jpg"
+const fullName = "Lizandro Vivanco Casamona";
+const currentYear = new Date().getFullYear();
+const profilePicture = "images/IMG_20211214.jpg";
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById("name");
+const nameElement = document.getElementById('name');
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
 const imageElement = document.querySelector("img"); 
-img.src = profilePicture;
 
 /* Step 4 - Adding Content */
 
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
-
-
-
-
-
-
+const favFoods = ["Ceviche", "Pollo a la Brasa", "Lomo Saltado", "Aji de Gallina", "Anticuchos"];
+foodElement.innerHTML = favFoods;
+const favFood = "Arroz Chaufa";
+favFoods.push(favFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
